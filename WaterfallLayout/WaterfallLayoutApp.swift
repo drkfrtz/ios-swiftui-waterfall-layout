edf25c7b27aa83eee4d086ddf7990d61d680a0ed
drkfrtz <drkfrtz@example.com>
2020-12-21 22:02:58 +0100

    Initial Commit

---
+++ WaterfallLayout/WaterfallLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterfallLayoutApp.swift
+//  WaterfallLayout
+//
+//  Created by Dirk Fritz on 21.12.20.
+//
+
+import SwiftUI
+
+@main
+struct WaterfallLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
